3fbfcb94fd13cbe1f63c2e4adcafa9ae2947b1f9
abdulmatnshah <abdulmateenshah@gmail.com>
2013-10-14 03:23:46 -0700

    turn on class for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ Election::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -20,14 +22,20 @@ Election::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+
+  # AMS - old code  
+  # config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #  AMS - Old code
+  # config.assets.compile = false
+  config.assets.compile = true
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +85,5 @@ Election::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
 end
